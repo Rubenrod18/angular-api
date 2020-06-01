81736143613f982d4ffb8d00516928f2905c93db
Rubén Rodríguez <rubenrod.018@gmail.com>
2020-06-01 23:35:55 +0200

    feat: add Angular skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
